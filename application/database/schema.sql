2bce517672113e02b7dbe64ae4da5ac88e9a5813
David McHugh <mcqman1@gmail.com>
2020-08-11 00:07:32 -0400

    Remove redundant code.

--- application/database/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `website`;
-
-CREATE TABLE IF NOT EXISTS `website`.`users` (`id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-                                              `email` VARCHAR(255) NOT NULL, 
-                                              `first_name` VARCHAR(255) NOT NULL,
-                                              `last_name` VARCHAR(255) NOT NULL,
-                                              `active` TINYINT(1) NOT NULL DEFAULT 0);
