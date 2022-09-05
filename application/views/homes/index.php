ff5ae59dfac02a4d8f5c0ada07449b00b18512d0
David McHugh <mcqman1@gmail.com>
2022-09-05 03:53:58 -0400

    Add model validation

--- application/views/homes/index.php
+++ application/views/homes/index.php
@@ -1,3 +1,3 @@
 <h2><?php echo APPLICATION_ROOT; ?></h2>
-<a href="/<?php echo APPLICATION_ROOT; ?>/account/signup">Create New Account</a><br>
-<a href="/<?php echo APPLICATION_ROOT; ?>/account/list">Account List</a>
+<a href="<?php echo route("/account/signup"); ?>">Create New Account</a><br>
+<a href="<?php echo route("/account/list"); ?>">Account List</a>
