aa28d71628eb081618989ff2ddefd6502dc76b01
David McHugh <mcqman1@gmail.com>
2022-09-10 22:50:24 -0400

    Move things around

--- application/framework/classes/database/Validation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface Validation 
-{
-    public function validates_presence_of(string $attribute);
-    public function validates_uniqueness_of(string $attribute);
-    public function validates_format_of(string $attribute, string $regex);
-    public function validates_length_of(string $attribute, int $minimum, int $maximum);
-}
