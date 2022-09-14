9880c4417c57650ddfe57e648ba82dc2164d8b44
David McHugh <mcqman1@gmail.com>
2022-09-13 22:15:12 -0400

    Fix issues with case sensitive filesystems

--- framework/configuration/initialize.php
+++ framework/configuration/initialize.php
@@ -2,8 +2,8 @@
 
 // Disable error reporting 
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
 // Enable sessions and output buffering
 
